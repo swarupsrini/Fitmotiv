da32b543f41cc7102ad0d8535a88287967eb0d1c
SaiG18 <saigurrapu18@gmail.com>
2021-01-16 15:09:49 -0500

    Xcode Project Init

---
+++ iOS/FitSync/FitSync/FitSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitSyncApp.swift
+//  FitSync
+//
+//  Created by Sai Gurrapu on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FitSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
