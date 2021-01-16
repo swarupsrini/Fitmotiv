2a48de0565df5270f5111351dcee3cd82dcc8b49
Swarup Srini <gswarup2001@gmail.com>
2021-01-16 18:23:44 -0500

    Fixed up DB stuff

--- Web/fitsync/routes/db.js
+++ Web/fitsync/routes/db.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const passwords = require("../keys/passwords.json");
+
 const fs = require("fs");
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "bilal",
-  password: fs.readFileSync(__dirname + "/../keys/db_pass.txt").toString(),
+  password: passwords["DB_PASS"],
   host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
   database: "defaultdb",
   port: 26257,
