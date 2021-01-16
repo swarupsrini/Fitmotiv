45c5d3050d7a107aabbf25498dc1ec3f89193f64
Swarup Srini <gswarup2001@gmail.com>
2021-01-16 17:31:21 -0500

    Set up CockroachDB stuff

---
+++ Web/fitsync/routes/db.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const fs = require("fs");
+const { Pool } = require("pg");
+
+console.log(fs.readFileSync(__dirname + "/../keys/db_pass.txt").toString());
+
+const pool = new Pool({
+  user: "bilal",
+  password: fs.readFileSync(__dirname + "/../keys/db_pass.txt").toString(),
+  host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
+  database: "defaultdb",
+  port: 26257,
+  ssl: {
+    ca: fs.readFileSync(__dirname + "/../keys/cc-ca.crt").toString(),
+  },
+});
+
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+
+router.get("/users", (req, res, next) => {});
+
+module.exports = router;
