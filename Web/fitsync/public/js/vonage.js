175d149a9cde0a71acb6023702466c2a9a91b0bd
Hemant Bhanot <hemant.bhanot01@gmail.com>
2021-01-16 19:58:07 -0500

    vonage api

--- Web/fitsync/public/js/vonage.js
+++ Web/fitsync/public/js/vonage.js
@@ -1,11 +1,3 @@
-// replace these values with those generated in your TokBox Account
-var apiKey = "47084544";
-var sessionId =
-  "1_MX40NzA4NDU0NH5-MTYxMDgyNzE5OTAzMn5wbVdycklHeVZUbVpmNnNKSklsU0FENlJ-fg";
-var token =
-  "T1==cGFydG5lcl9pZD00NzA4NDU0NCZzaWc9NDkyZTkzNzg0OTFjYTA5NWJkMmNjMGU1OTMwNzllODUwY2VkZGMyYjpzZXNzaW9uX2lkPTFfTVg0ME56QTRORFUwTkg1LU1UWXhNRGd5TnpFNU9UQXpNbjV3YlZkeWNrbEhlVlpVYlZwbU5uTktTa2xzVTBGRU5sSi1mZyZjcmVhdGVfdGltZT0xNjEwODI3OTA0Jm5vbmNlPTAuMzU3ODc0NzIwMTQ1MzM3NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjEwODMxNTA0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
-
-// (optional) add server code here
 initializeSession();
 
 // Handling all of our errors here by alerting them
@@ -16,8 +8,9 @@ function handleError(error) {
 }
 
 function initializeSession() {
-  var session = OT.initSession(apiKey, sessionId);
-
+  console.log(data);
+  console.log(data.sessionID);
+  let session = OT.initSession(data.api_key, data.sessionID);
   // Subscribe to a newly created stream
   session.on("streamCreated", function (event) {
     session.subscribe(
@@ -44,7 +37,7 @@ function initializeSession() {
   );
 
   // Connect to the session
-  session.connect(token, function (error) {
+  session.connect(data.token, function (error) {
     // If the connection is successful, publish to the session
     if (error) {
       handleError(error);
--- Web/fitsync/routes/index.js
+++ Web/fitsync/routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const OpenTok = require("opentok");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const secrets = require("../keys/passwords.json");
+
+const API_KEY = secrets["API_KEY"];
+const API_SECRET = secrets["SECRET"];
+
+router.get("/", function (req, res, next) {
+  const vonage_creds = create_session();
+  console.log(vonage_creds);
+  let json = {
+    api_key: API_KEY,
+    sessionID: vonage_creds.sessionID,
+    token: vonage_creds["token"],
+  };
+  res.render("index", { data: JSON.stringify(json) });
 });
+
+function create_session() {
+  const opentok = new OpenTok(API_KEY, API_SECRET);
+  let sessionId;
+  let token;
+  opentok.createSession({ mediaMode: "routed" }, function (error, session) {
+    if (error) {
+      console.log("Error creating session:", error);
+    } else {
+      sessionId = session.sessionId;
+      let tokenOptions = {};
+      tokenOptions.role = "publisher";
+
+      // Generate a token.
+      token = opentok.generateToken(sessionId, tokenOptions);
+    }
+  });
+  // console.log(`token:: ${token}`);
+  sessionId =
+    "2_MX40NzA4NDU0NH5-MTYxMDg0MzcwODkwN35IMW9xLzBqN3E0My8xSkwzbmNaZ09Scmd-fg";
+  token =
+    "T1==cGFydG5lcl9pZD00NzA4NDU0NCZzaWc9MGMzYmM1YTFlMTg1NzhhNDk4N2ZjNjYxZThiYTU4Nzg2Y2QyNTMyNTpzZXNzaW9uX2lkPTJfTVg0ME56QTRORFUwTkg1LU1UWXhNRGcwTXpjd09Ea3dOMzVJTVc5eEx6QnFOM0UwTXk4eFNrd3pibU5hWjA5U2NtZC1mZyZjcmVhdGVfdGltZT0xNjEwODQzNzI0Jm5vbmNlPTAuODAyODYxNTE0MzU1NTYwMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjEzNDM1NzIzJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+  return { sessionID: sessionId, token: token };
+}
 
 module.exports = router;
