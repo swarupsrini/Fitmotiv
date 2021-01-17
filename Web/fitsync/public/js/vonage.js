b56bf180eee917c1e1cfbfd3ec7e990b65075969
Swarup Srini <gswarup2001@gmail.com>
2021-01-16 23:12:13 -0500

    Finished vid call website

--- Web/fitsync/public/js/vonage.js
+++ Web/fitsync/public/js/vonage.js
@@ -1,3 +1,7 @@
+let publisher;
+let vidOn = true;
+let micOn = true;
+
 initializeSession();
 
 // Handling all of our errors here by alerting them
@@ -8,8 +12,6 @@ function handleError(error) {
 }
 
 function initializeSession() {
-  console.log(data);
-  console.log(data.sessionID);
   let session = OT.initSession(data.api_key, data.sessionID);
   // Subscribe to a newly created stream
   session.on("streamCreated", function (event) {
@@ -20,18 +22,22 @@ function initializeSession() {
         insertMode: "append",
         width: "100%",
         height: "100%",
+        style: { buttonDisplayMode: "off" },
       },
       handleError
     );
   });
 
   // Create a publisher
-  var publisher = OT.initPublisher(
+  publisher = OT.initPublisher(
     "publisher",
     {
       insertMode: "append",
       width: "100%",
       height: "100%",
+      style: { buttonDisplayMode: "off" },
+      publishAudio: true,
+      publishVideo: true,
     },
     handleError
   );
@@ -45,4 +51,30 @@ function initializeSession() {
       session.publish(publisher, handleError);
     }
   });
+}
+
+function videobtnclick(event) {
+  vidOn = !vidOn;
+  console.log(vidOn);
+  publisher.publishVideo(vidOn);
+  if (vidOn) {
+    document.getElementById("videobtn").innerHTML =
+      '<span class="btnicon iconify" data-icon="bx:bx-video"></span>';
+  } else {
+    document.getElementById("videobtn").innerHTML =
+      '<span class="btnicon iconify" data-icon="bx:bx-video-off"></span>';
+  }
+}
+
+function audiobtnclick(event) {
+  micOn = !micOn;
+  console.log(micOn);
+  publisher.publishAudio(micOn);
+  if (micOn) {
+    document.getElementById("audiobtn").innerHTML =
+      '<span class="btnicon iconify" data-icon="bx:bx-microphone"></span>';
+  } else {
+    document.getElementById("audiobtn").innerHTML =
+      '<span class="btnicon iconify" data-icon="bx:bx-microphone-off"></span>';
+  }
 }
--- Web/fitsync/routes/index.js
+++ Web/fitsync/routes/index.js
@@ -8,17 +8,17 @@ const API_KEY = secrets["API_KEY"];
 const API_SECRET = secrets["SECRET"];
 
 router.get("/", function (req, res, next) {
-  const vonage_creds = create_session();
-  console.log(vonage_creds);
-  let json = {
-    api_key: API_KEY,
-    sessionID: vonage_creds.sessionID,
-    token: vonage_creds["token"],
-  };
-  res.render("index", { data: JSON.stringify(json) });
+  create_session(({ sessionId, token }) => {
+    let json = {
+      api_key: API_KEY,
+      sessionID: sessionId,
+      token: token,
+    };
+    res.render("index", { data: JSON.stringify(json) });
+  });
 });
 
-function create_session() {
+function create_session(callback) {
   const opentok = new OpenTok(API_KEY, API_SECRET);
   let sessionId;
   let token;
@@ -30,16 +30,16 @@ function create_session() {
       let tokenOptions = {};
       tokenOptions.role = "publisher";
 
-      // Generate a token.
       token = opentok.generateToken(sessionId, tokenOptions);
+
+      sessionId =
+        "2_MX40NzA4NDU0NH5-MTYxMDg0MzcwODkwN35IMW9xLzBqN3E0My8xSkwzbmNaZ09Scmd-fg";
+      token =
+        "T1==cGFydG5lcl9pZD00NzA4NDU0NCZzaWc9MGMzYmM1YTFlMTg1NzhhNDk4N2ZjNjYxZThiYTU4Nzg2Y2QyNTMyNTpzZXNzaW9uX2lkPTJfTVg0ME56QTRORFUwTkg1LU1UWXhNRGcwTXpjd09Ea3dOMzVJTVc5eEx6QnFOM0UwTXk4eFNrd3pibU5hWjA5U2NtZC1mZyZjcmVhdGVfdGltZT0xNjEwODQzNzI0Jm5vbmNlPTAuODAyODYxNTE0MzU1NTYwMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjEzNDM1NzIzJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+
+      callback({ sessionId, token });
     }
   });
-  // console.log(`token:: ${token}`);
-  sessionId =
-    "2_MX40NzA4NDU0NH5-MTYxMDg0MzcwODkwN35IMW9xLzBqN3E0My8xSkwzbmNaZ09Scmd-fg";
-  token =
-    "T1==cGFydG5lcl9pZD00NzA4NDU0NCZzaWc9MGMzYmM1YTFlMTg1NzhhNDk4N2ZjNjYxZThiYTU4Nzg2Y2QyNTMyNTpzZXNzaW9uX2lkPTJfTVg0ME56QTRORFUwTkg1LU1UWXhNRGcwTXpjd09Ea3dOMzVJTVc5eEx6QnFOM0UwTXk4eFNrd3pibU5hWjA5U2NtZC1mZyZjcmVhdGVfdGltZT0xNjEwODQzNzI0Jm5vbmNlPTAuODAyODYxNTE0MzU1NTYwMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjEzNDM1NzIzJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
-  return { sessionID: sessionId, token: token };
 }
 
 module.exports = router;
