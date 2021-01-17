cd2a61606e27a2b4289a8d10e705c42d06b1519c
Swarup Srini <gswarup2001@gmail.com>
2021-01-17 04:09:20 -0500

    Finished IBM API integration

--- Web/fitsync/public/js/vonage.js
+++ Web/fitsync/public/js/vonage.js
@@ -11,7 +11,7 @@ function init_sdk() {
   if (!!window.SpeechSDK) {
     sdk = window.SpeechSDK;
   }
-  // azure_speech();
+  azure_speech();
 }
 // Handling all of our errors here by alerting them
 function handleError(error) {
@@ -69,12 +69,44 @@ function azure_speech() {
   recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
   console.log("Speak into your microphone.");
+
+  // recognizer.recognizing = (s, e) => {};
+
   recognizer.recognized = (s, e) => {
     console.log(`RECOGNIZED: Text=${e.result.text}`);
+    const txt = e.result.text.toLowerCase();
+
+    console.log(txt);
+    console.log(txt.includes("video"));
+    if (txt.includes("mute")) {
+      audiobtnclick();
+    } else if (txt.includes("video")) {
+      videobtnclick();
+    }
+
+    fetch(
+      "https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/a1c64d0b-5d55-4c24-9d0a-9ad7251b630c/v1/analyze?version=2019-07-12",
+      {
+        body: JSON.stringify({
+          text: txt,
+          features: {
+            sentiment: { targets: ["pause"] },
+            keywords: { emotion: true },
+          },
+        }),
+        headers: {
+          Authorization: data.ibm,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
-  recognizer.recognizing = (s, e) => {
-    console.log(`RECOGNIZING: Text=${e.result.text}`);
-  };
+
   recognizer.startContinuousRecognitionAsync();
 }
 
@@ -92,15 +124,21 @@ function videobtnclick(event) {
 }
 
 function audiobtnclick(event) {
-  azure_speech();
   micOn = !micOn;
   console.log(micOn);
   publisher.publishAudio(micOn);
   if (micOn) {
+    azure_speech();
+    recognizer.startContinuousRecognitionAsync();
     document.getElementById("audiobtn").innerHTML =
       '<span class="btnicon iconify" data-icon="bx:bx-microphone"></span>';
   } else {
+    // recognizer.stopContinuousRecognitionAsync();
     document.getElementById("audiobtn").innerHTML =
       '<span class="btnicon iconify" data-icon="bx:bx-microphone-off"></span>';
   }
 }
+
+window.addEventListener("beforeunload", function (event) {
+  recognizer.stopContinuousRecognitionAsync();
+});
